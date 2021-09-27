ff96888756fc04853f04d6c229290afa4fb8c4d3
KuruZero <jordyjonat@yahoo.co.jp>
2021-09-27 16:44:05 +0900

    test file

---
+++ Assets/Script/Jordy/test.cs
@@ -0,0 +1,39 @@
+﻿using Packages.Rider.Editor.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wrapper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+
+}
+
+class A
+{
+    public virtual void MyUpdate()
+    {
+        Debug.Log("A");
+    }
+}
+
+class B : A
+{
+    public override void MyUpdate()
+    {
+        base.MyUpdate();
+        Debug.Log("B");
+    }
+}

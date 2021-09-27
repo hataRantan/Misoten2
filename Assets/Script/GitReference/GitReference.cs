89a34df94f49c5e3e1526076de2dc4457084b30e
hata <hata202012@gmail.com>
2021-09-27 16:12:36 +0900

    プルテストのための変更

--- Assets/Script/GitReference/GitReference.cs
+++ Assets/Script/GitReference/GitReference.cs
@@ -7,7 +7,7 @@ public class GitReference : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("プルテストのための奴");
     }
 
     // Update is called once per frame

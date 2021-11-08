b4bea25a60551d611e3220bae7d46fad6ebc9227
hata <hata202012@gmail.com>
2021-11-08 14:44:49 +0900

    アイテム出現テスト

---
+++ Assets/Script/Hata/Item/Manager/MyItemDeleteAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyItemDeleteAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    private float timer = 10.0f;
+    float currentTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime<timer)
+        {
+            currentTime += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Script/Hata/Item/Manager/MyItemManager.cs
+++ Assets/Script/Hata/Item/Manager/MyItemManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MyItemManager : MyUpdater
 {
     //アイテムのランク
-    //Numは通常使わない
     public enum Rank
     {
         S,A,B,C,D
@@ -20,6 +19,8 @@ public class MyItemManager : MyUpdater
 
     [Header("出現させるアイテム一覧")]
     [SerializeField] List<RankItem> createItem = new List<RankItem>();
+    //ランクごとにアイテム
+    Dictionary<Rank, List<GameObject>> rankItems = new Dictionary<Rank, List<GameObject>>();
 
     [Header("床管理クラス")]
     [SerializeField] FloorManager floorManager = null;
@@ -43,15 +44,65 @@ public class MyItemManager : MyUpdater
     //プレイヤーの体力監視
 
     //通常は序盤、中盤、終盤でアイテムの出現具合をばらけさせる
-    
-    //アイテム管理の状態マシーン
-    private IStateSpace.StateMachineBase<MyGameProgress.GameProgress, MyPlayerObject> m_machine;
 
+    //アイテム管理の状態マシーン
+    private IStateSpace.StateMachineBase<MyGameProgress.GameProgress, MyItemManager> m_machine;
+
+    //ToDo：後で消すことテスト用
+    [SerializeField]
+    MyItemAppearTest _testCanvas = null;
 
     public override void MySecondInit()
     {
-        //状態マシーン初期化
+        //それぞれのランクのアイテムの個数を求める
+        List<GameObject> sRankItems = new List<GameObject>();
+        List<GameObject> aRankItems = new List<GameObject>();
+        List<GameObject> bRankItems = new List<GameObject>();
+        List<GameObject> cRankItems = new List<GameObject>();
+        List<GameObject> dRankItems = new List<GameObject>();
+        
+        foreach (var rankItem in createItem)
+        {
+            switch(rankItem.rank)
+            {
+                case Rank.S:
+                    sRankItems.Add(rankItem.item);
+                    break;
 
+                case Rank.A:
+                    aRankItems.Add(rankItem.item);
+                    break;
+
+                case Rank.B:
+                    bRankItems.Add(rankItem.item);
+                    break;
+
+                case Rank.C:
+                    cRankItems.Add(rankItem.item);
+                    break;
+
+                case Rank.D:
+                    dRankItems.Add(rankItem.item);
+                    break;
+            }
+        }
+
+        //ランクごとのListをDictionaryにセットする
+        rankItems.Add(Rank.S, sRankItems);
+        rankItems.Add(Rank.A, aRankItems);
+        rankItems.Add(Rank.B, bRankItems);
+        rankItems.Add(Rank.C, cRankItems);
+        rankItems.Add(Rank.D, dRankItems);
+
+
+        //状態マシーン初期化
+        m_machine = new IStateSpace.StateMachineBase<MyGameProgress.GameProgress, MyItemManager>();
+        m_machine.AddState(MyGameProgress.GameProgress.BEGINNING, new MyItemBeggin(), this);
+        m_machine.AddState(MyGameProgress.GameProgress.MIDDLE, new MyItemMiddle(), this);
+        m_machine.AddState(MyGameProgress.GameProgress.FINAL, new MyItemFinal(), this);
+
+        //初期状態
+        m_machine.InitState(MyGameProgress.GameProgress.BEGINNING);
     }
 
     public override void MyUpdate()
@@ -74,19 +125,62 @@ public class MyItemManager : MyUpdater
     {
         public override void Entry()
         {
-
+            //現在の進行状況を設定
+            board.m_chance.SetProgress(MyGameProgress.GameProgress.BEGINNING);
         }
 
-        public override void Exit()
-        {
-        }
+        public override void Exit() { }
 
         public override MyGameProgress.GameProgress Update()
         {
+            if (board.gameProgress.Progress == MyGameProgress.GameProgress.MIDDLE)
+                return MyGameProgress.GameProgress.MIDDLE;
+
             return MyGameProgress.GameProgress.BEGINNING;
         }
     }
 
+    /// <summary>
+    /// 中盤のアイテム出現
+    /// </summary>
+    public class MyItemMiddle : IStateSpace.IState<MyGameProgress.GameProgress, MyItemManager>
+    {
+        public override void Entry()
+        {
+            //現在の進行状況を設定
+            board.m_chance.SetProgress(MyGameProgress.GameProgress.MIDDLE);
+        }
+
+        public override void Exit() { }
+
+        public override MyGameProgress.GameProgress Update()
+        {
+            if (board.gameProgress.Progress == MyGameProgress.GameProgress.FINAL)
+                return MyGameProgress.GameProgress.FINAL;
+
+            return MyGameProgress.GameProgress.MIDDLE;
+        }
+    }
+
+    /// <summary>
+    /// 終盤のアイテム出現
+    /// </summary>
+    public class MyItemFinal : IStateSpace.IState<MyGameProgress.GameProgress, MyItemManager>
+    {
+        public override void Entry()
+        {
+            //現在の進行状況を設定
+            board.m_chance.SetProgress(MyGameProgress.GameProgress.FINAL);
+        }
+
+        public override void Exit() { }
+
+        public override MyGameProgress.GameProgress Update()
+        {
+            
+            return MyGameProgress.GameProgress.FINAL;
+        }
+    }
 
     /// <summary>
     /// 進行状況クラスを取得する
@@ -94,5 +188,24 @@ public class MyItemManager : MyUpdater
     public void SetGameProgress(MyGameProgress _progress)
     {
         gameProgress = _progress;
+    }
+
+
+    /// <summary>
+    /// ランクから出現させるアイテムを取得する
+    /// </summary>
+    private GameObject GetAppearItem(Rank _rank)
+    {
+        //指定のランクのアイテムの個数を取得
+        int itemNum = rankItems[_rank].Count;
+
+        if(itemNum <=0) //エラーチェック
+        {
+            Debug.LogError("MyItemManager：指定したランクのアイテムがありません");
+            return null;
+        }
+
+        //ToDo：同じランクのアイテムの出現は、現状同じ
+        return rankItems[_rank][Random.Range(0, itemNum)];
     }
 }

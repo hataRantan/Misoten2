e0b61811beabdaf31c72c59e99ea40466ccba27d
WIN10OHS80619\ohs80619 <myuua0427@gmail.com>
2021-11-25 13:58:36 +0900

    タイトルシーンα追加

---
+++ Assets/Script/muraki/BackGround.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyTitleStateClass
+{
+    public class tAnimationState : IStateSpace.IState<MyTitleObject.MyTitleObjectState, MyTitleObject>
+    {
+        //正規化した移動方向
+        Vector3 m_moveDirect = Vector3.zero;
+
+        public override void Entry()
+        {
+            board.CallTest();
+        }
+        public override void Exit() { }
+        
+
+        public override MyTitleObject.MyTitleObjectState Update()
+        {
+            return MyTitleObject.MyTitleObjectState.tMOVE;
+        }
+    }
+
+
+
+
+    public class tDeadState : IStateSpace.IState<MyTitleObject.MyTitleObjectState, MyTitleObject>
+    {
+        public override void Entry()
+        {
+            //ToDo：dead処理の呼び出し
+        }
+
+        public override void Exit()
+        {
+        }
+
+        public override MyTitleObject.MyTitleObjectState Update()
+        {
+            return MyTitleObject.MyTitleObjectState.tDEAD;
+        }
+    }
+
+}
